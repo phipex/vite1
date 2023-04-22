f0585d981f6219ffa2b9f8cd227037f8afd66637
Andres Felipe (@phipex) Montoya <andres.montoya@iesonline.com.co>
2023-04-22 11:56:53 -0500

    chore: configuracion initial

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import "./style.css";
+import { sum } from "./sum";
 
 export { sum } from "./sum";
+
+console.log(sum(1, 2));
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,28 @@
 /// <reference types="vitest" />
-import path from "path";
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from "vite";
+import legacy from '@vitejs/plugin-legacy';
 import packageJson from "./package.json";
 
-const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
-
-const fileName = {
-  es: `${getPackageName()}.mjs`,
-  cjs: `${getPackageName()}.cjs`,
-  iife: `${getPackageName()}.iife.js`,
-};
-
-const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 module.exports = defineConfig({
   base: "./",
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
-      formats,
-      fileName: (format) => fileName[format],
-    },
-  },
+
   test: {
 
-  }
+  },
+  plugins: [
+
+    legacy({
+      targets: {
+        firefox: '49'
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
 });
