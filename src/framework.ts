1e1136a1cb51ec3ea4716e83d0a2a5d283f258aa
andres.montoya <andres.montoya@iesonline.com.co>
2023-05-09 15:41:22 -0500

    feat: framework v1

---
+++ src/framework.ts
@@ -0,0 +1,89 @@
+// Definición de la interfaz de la vista
+export interface View {
+  render(): void;
+}
+
+// Clase Observable para implementar el enfoque reactivo
+export class Observable<T> {
+  private observers: Array<(value: T) => void> = [];
+
+  subscribe(observer: (value: T) => void): void {
+    this.observers.push(observer);
+  }
+
+  unsubscribe(observer: (value: T) => void): void {
+    this.observers = this.observers.filter(obs => obs !== observer);
+  }
+
+  notify(value: T): void {
+    this.observers.forEach(obs => obs(value));
+  }
+}
+
+// Clase Model que se encarga de manejar el estado de la aplicación
+export class Model<T> {
+  private state: T;
+
+  private state$ = new Observable<T>();
+
+  constructor(initialState: T) {
+    this.state = initialState;
+  }
+
+  getState(): T {
+    return this.state;
+  }
+
+  setState(newState: T): void {
+    this.state = newState;
+    this.state$.notify(this.state);
+  }
+
+  getState$(): Observable<T> {
+    return this.state$;
+  }
+}
+
+// Clase Presenter que se encarga de manejar la lógica de la aplicación
+export class Presenter<T> {
+  private view: View;
+
+  private model: Model<T>;
+
+  constructor(view: View, model: Model<T>) {
+    this.view = view;
+    this.model = model;
+
+    // Suscribirse al Observable del modelo para actualizar la vista en caso de cambio
+    this.model.getState$().subscribe(() => {
+      this.view.render();
+    });
+  }
+
+  getView(): View {
+    return this.view;
+  }
+
+  getModel(): Model<T> {
+    return this.model;
+  }
+}
+
+// Clase Component que define la base de cualquier componente que desee utilizar el framework
+export abstract class Component<T> {
+  private presenter: Presenter<T>;
+
+  constructor(view: View, model: Model<T>) {
+    this.presenter = new Presenter(view, model);
+  }
+
+  protected getView(): View {
+    return this.presenter.getView();
+  }
+
+  protected getModel(): Model<T> {
+    return this.presenter.getModel();
+  }
+
+  abstract render(): void;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
+import { Component, Model } from "./framework";
 import "./style.css";
 import { sum } from "./sum";
 
 export { sum } from "./sum";
 
 console.log(sum(1, 2));
+
+// Ejemplo de implementación del framework en un componente de contador
+class CounterComponent extends Component<number> {
+  constructor() {
+    // Crear la vista
+    const view = {
+      render: () => {
+        const value = this.getModel().getState();
+        console.log(`El contador tiene un valor de ${value}`);
+      },
+    };
+
+    // Crear el modelo
+    const model = new Model<number>(0);
+
+    super(view, model);
+  }
+
+  // Método para incrementar el contador
+  increment(): void {
+    const currentValue = this.getModel().getState();
+    this.getModel().setState(currentValue + 1);
+  }
+
+  // Método para decrementar el contador
+  decrement(): void {
+    const currentValue = this.getModel().getState();
+    this.getModel().setState(currentValue - 1);
+  }
+
+  // Método para renderizar el componente
+  render(): void {
+    this.getView().render();
+  }
+}
+
+// Crear una instancia del componente de contador
+const counter = new CounterComponent();
+
+// Incrementar el contador dos veces
+counter.increment();
+counter.increment();
+
+// Decrementar el contador una vez
+counter.decrement();
+
+// Renderizar el componente para actualizar la vista
+counter.render();
